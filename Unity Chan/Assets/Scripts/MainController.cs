716fe2cc54e0b2b0896c1e8da9d7f8d84a55e790
BoGyuPark <shaing123@naver.com>
2018-08-07 21:20:10 +0900

    튜토리얼 10~12
    Unity Chan 캐릭터 애니메이션 및 캐릭터 충돌 감지 적용

--- Unity Chan/Assets/Scripts/MainController.cs
+++ Unity Chan/Assets/Scripts/MainController.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 public class MainController : MonoBehaviour {
 
     public Animator animator;
+    public Rigidbody rigidbody;
+    private float h;
+    private float v;
 
+    private float moveX;
+    private float moveZ;
+    private float speedH = 50f;
+    private float speedZ = 80f;
 
-	// Use this for initialization
-	void Start () {
-        animator = GetComponent<Animator>();	
+    // Use this for initialization
+    void Start () {
+        animator = GetComponent<Animator>();
+        rigidbody = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
@@ -18,5 +26,49 @@ public class MainController : MonoBehaviour {
         {
             animator.Play("JUMP00", -1, 0);
         }
-	}
+
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+
+        animator.SetFloat("h", h);
+        animator.SetFloat("v", v);
+
+        moveX = h * speedH * Time.deltaTime;
+        moveZ = v * speedZ * Time.deltaTime;
+
+        if (moveZ <= 0)
+            moveX = 0;
+
+        rigidbody.velocity = new Vector3(moveX, 0, moveZ);
+
+
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Cube")
+        {
+            Debug.Log("충돌 감지");
+            animator.Play("DAMAGED01", -1, 0);
+            this.transform.Translate(Vector3.back * speedZ * Time.deltaTime);
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.collider.tag == "Cube")
+        {
+            Debug.Log("충돌 유지");
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.collider.tag == "Cube")
+        {
+            Debug.Log("충돌 종료");
+        }
+    }
+
 }
