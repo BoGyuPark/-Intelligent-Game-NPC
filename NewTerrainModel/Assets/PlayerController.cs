c736a50539ff9168412e19911bafd7c050c78c27
BoGyuPark <shaing123@naver.com>
2018-09-13 16:59:47 +0900

    Revert "Player Movement 수정"
    This reverts commit b5081059d5c3e94529e9724dc5dd31b1dc96963b.

--- NewTerrainModel/Assets/PlayerController.cs
+++ NewTerrainModel/Assets/PlayerController.cs
@@ -2,26 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
 
     public float movementSpeed;
     public float velocity;
     public Rigidbody rb;
-    public Animator anim;
 
-    // Use this for initialization
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
         GetInput();
         Move();
-    }
+	}
 
     void GetInput()
     {
@@ -30,7 +26,7 @@ public class PlayerController : MonoBehaviour
         {
             SetVelocity(-1);
         }
-        else if (Input.GetKeyUp(KeyCode.A))
+        else if(Input.GetKeyUp(KeyCode.A))
         {
             SetVelocity(0);
         }
@@ -39,7 +35,7 @@ public class PlayerController : MonoBehaviour
         {
             SetVelocity(1);
         }
-        else if (Input.GetKeyUp(KeyCode.D))
+        else if(Input.GetKeyUp(KeyCode.D))
         {
             SetVelocity(0);
 
@@ -48,28 +44,10 @@ public class PlayerController : MonoBehaviour
 
     void Move()
     {
-        if (velocity == 0)
-        {
-            anim.SetInteger("Condition", 0);
-            return;
-        }
-        else
-        {
-            anim.SetInteger("Condition", 1);
-        }
         rb.MovePosition(transform.position + (Vector3.right * velocity * movementSpeed * Time.deltaTime));
     }
     void SetVelocity(float dir)
     {
-        if (dir < 0)
-        {
-            transform.LookAt(transform.position + Vector3.left);
-        }
-        else if (dir > 0)
-        {
-            transform.LookAt(transform.position + Vector3.right);
-        }
-
         velocity = dir;
     }
 }
